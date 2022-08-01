maan2003/lsp_lines.nvim

commit 8b4e9fa9df1ceaef42c20c1f29dd565f55b7b812

Add only_current_line option
This patch adds an `only_current_line` option similar to the virtual
text handler - only shows the diagnostics for the current line.

This also changes the annotation for the opts parameter to the `show`
function from boolean to table because it is the entire table
passed to vim.diagnostic.config, including other handlers

lua/lsp_lines/init.lua
@@ -2,9 +2,22 @@ local M = {}
 
 local render = require("lsp_lines.render")
 
+local function render_current_line(diagnostics, ns, bufnr, opts)
+  local current_line_diag = {}
+  local lnum = vim.api.nvim_win_get_cursor(0)[1] - 1
+
+  for _, diagnostic in pairs(diagnostics) do
+    local show = diagnostic.end_lnum and (lnum >= diagnostic.lnum and lnum <= diagnostic.end_lnum) or (lnum == diagnostic.lnum)
+    if show then table.insert(current_line_diag, diagnostic) end
+  end
+
+  render.show(ns, bufnr, current_line_diag, opts)
+end
+
 -- Registers a wrapper-handler to render lsp lines.
 -- This should usually only be called once, during initialisation.
 M.setup = function()
+  vim.api.nvim_create_augroup("LspLines", { clear = true })
   -- TODO: On LSP restart (e.g.: diagnostics cleared), errors don't go away.
   vim.diagnostic.handlers.virtual_lines = {
     ---@param namespace number
@@ -16,7 +29,20 @@ M.setup = function()
       if not ns.user_data.virt_lines_ns then
         ns.user_data.virt_lines_ns = vim.api.nvim_create_namespace("")
       end
-      render.show(ns.user_data.virt_lines_ns, bufnr, diagnostics, opts)
+
+      vim.api.nvim_clear_autocmds({ group = "LspLines" })
+      if opts.virtual_lines.only_current_line then
+        vim.api.nvim_create_autocmd("CursorMoved", {
+          buffer = bufnr,
+          callback = function ()
+            render_current_line(diagnostics, ns.user_data.virt_lines_ns, bufnr, opts)
+          end
+        })
+        -- Also show diagnostics for the current line before the first CursorMoved event
+        render_current_line(diagnostics, ns.user_data.virt_lines_ns, bufnr, opts)
+      else
+        render.show(namespace, bufnr, diagnostics, opts)
+      end
     end,
     ---@param namespace number
     ---@param bufnr number
@@ -24,6 +50,7 @@ M.setup = function()
       local ns = vim.diagnostic.get_namespace(namespace)
       if ns.user_data.virt_lines_ns then
         render.hide(ns.user_data.virt_lines_ns, bufnr )
+        vim.api.nvim_clear_autocmds({ group = "LspLines" })
       end
     end,
   }

commit 0fc1bd02d11b33d5bf1ee6bff3d66cd43d1b66d1

Fix typo/regression introduced in 8b4e9fa
Fixes: https://todo.sr.ht/~whynothugo/lsp_lines.nvim/18

lua/lsp_lines/init.lua
@@ -41,7 +41,7 @@ M.setup = function()
         -- Also show diagnostics for the current line before the first CursorMoved event
         render_current_line(diagnostics, ns.user_data.virt_lines_ns, bufnr, opts)
       else
-        render.show(namespace, bufnr, diagnostics, opts)
+        render.show(ns.user_data.virt_lines_ns, bufnr, diagnostics, opts)
       end
     end,
     ---@param namespace number